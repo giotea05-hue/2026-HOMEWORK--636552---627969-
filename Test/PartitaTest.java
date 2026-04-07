d3c4eb5a38e07c976f375f08741074bf678fa239
giotea05-hue <giotea05@gmail.com>
2026-04-07 17:13:54 +0200

    Refactor PartitaTest to use IOConsole

--- Test/PartitaTest.java
+++ Test/PartitaTest.java
@@ -1,33 +1,38 @@
 
 
+package HomeworkA;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import it.uniroma3.diadia.IOConsole;
 import it.uniroma3.diadia.Partita;
 import it.uniroma3.diadia.ambienti.Labirinto;
 import it.uniroma3.diadia.ambienti.Stanza;
 import it.uniroma3.diadia.giocatore.Giocatore;
 
 class PartitaTest {
+	
+	private IOConsole io=new IOConsole();
 
 	//TEST GET STANZA CORRENTE 
 	@Test
 	void testGetStanzaCorrente_null(){
-		Partita p= new Partita();
+		Partita p= new Partita(io);
 		p.setStanzaCorrente(null);
 		assertEquals(p.getStanzaCorrente(), null);
 	}
 	
 	@Test
 	void testGetStanzaCorrente_nonSettata() {
-		Partita p= new Partita();
+		Partita p= new Partita(io);
 		assertEquals(p.getStanzaCorrente().getNome(),"Atrio");
 	}
 	
 	@Test
 	void testGetStanzaCorrente(){
-		Partita p= new Partita();
+		Partita p= new Partita(io);
 		Stanza s= new Stanza("s");
 		p.setStanzaCorrente(s);
 		assertEquals(p.getStanzaCorrente(), s);
@@ -37,13 +42,13 @@ class PartitaTest {
 	
 	@Test
 	void testVinta_nonSettata(){
-		Partita p= new Partita();
+		Partita p= new Partita(io);
 		assertFalse(p.vinta());
 	}
 	
 	@Test
 	void testVinta(){
-		Partita p= new Partita();
+		Partita p= new Partita(io);
 		Stanza s= new Stanza("s");
 		p.setStanzaCorrente(s);
 		assertFalse(p.vinta());
@@ -51,7 +56,7 @@ class PartitaTest {
 	
 	@Test
 	void testVinta_vinta(){
-		Partita p= new Partita();
+		Partita p= new Partita(io);
 		Labirinto l=p.getLabirinto();
 		l.creaStanze();
 		p.setStanzaCorrente(l.getStanzaVincente());
@@ -61,20 +66,20 @@ class PartitaTest {
 	//TEST IS FINITA
 	@Test
 	void testIsFinita_nonFinita(){
-		Partita p= new Partita();
+		Partita p= new Partita(io);
 		assertFalse(p.isFinita());
 	}
 	
 	@Test
 	void testIsFinita_CFUFiniti(){
-		Partita p= new Partita();
+		Partita p= new Partita(io);
 		p.getPlayer().setCFU(0);
 		assertTrue(p.isFinita());
 	}
 	
 	@Test
 	void testIsFinita_settataFinita(){
-		Partita p= new Partita();
+		Partita p= new Partita(io);
 		p.setFinita();
 		assertTrue(p.isFinita());
 	}
