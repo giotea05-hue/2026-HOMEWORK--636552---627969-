c9bebe11108d3ce4d05eba71f5aa0ef5a38fea7d
SilviaCarriero <silvia.carriero28@gmail.com>
2026-04-07 16:32:14 +0200

    Update Borsa.java

--- src/it/uniroma3/diadia/giocatore/Borsa.java
+++ src/it/uniroma3/diadia/giocatore/Borsa.java
@@ -36,7 +36,7 @@ public class Borsa {
         return true;
     }
     
-    public Attrezzo removeAttrezzo(Attrezzo attrezzo) {//MODIFICA STRING IN ATTREZZO E RIDUCI PESO BORSA
+    public Attrezzo removeAttrezzo(Attrezzo attrezzo) {
         Attrezzo a = null;
         for(int i=0;i<this.numeroAttrezzi;i++) {
         	if(this.attrezzi[i]==attrezzo) {
@@ -58,7 +58,7 @@ public class Borsa {
     public Attrezzo getAttrezzo(String nomeAttrezzo) {
         Attrezzo a = null;
         for (int i = 0; i < this.numeroAttrezzi; i++)
-            if (this.attrezzi[i].getNome().equals(nomeAttrezzo))//PRENDE L'ATTREZZO DALLA BORSA
+            if (this.attrezzi[i].getNome().equals(nomeAttrezzo))
                 a = attrezzi[i];
 
         return a;
